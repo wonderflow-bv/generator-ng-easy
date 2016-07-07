4eb4ec4aaf370b3c3ea799f8c544918c883dc636
korut94 <andreamnt94@gmail.com>
2016-07-07 11:50:17 +0200

    Add features + Get interaction with the user + Write file bower.json and package.json + Install dependencies

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,16 +1,113 @@
+var ejs = require('ejs');
+var fs = require('fs');
 var generators = require('yeoman-generator');
 
+var bowConfig = require('./templates/bower.js');
+
+/**
+ * Put into an array the name of dependencies and dev-dependencies as a bower
+ * module location
+ */
+function generateDependencies() {
+    var dependencies = [];
+    var asBower = function (name) {
+	return 'bower_compontens/' + name + '/' + name +'.js';
+    };
+
+    for (var d in bowConfig.dependencies) {
+	dependencies.push(asBower(d));
+    }
+
+    for (var dD in bowConfig.devDependencies) {
+	dependencies.push(asBower(dD));
+    }
+
+    // The module to testing
+    dependencies.push(this.module.name + '.js');
+
+    return dependencies;
+}
+
+/**
+ * Writhe the bower.json in the root of your module
+ */
+function writeBowerJson() {
+    var self = this;
+
+    var bowerContent = JSON.stringify(bowConfig, null, 2);
+    // Instantiate the bower.json with the module configuration
+    var bowerRender = ejs.render(bowerContent, {module: this.module});
+    fs.writeFile('bower.json', bowerRender, 'utf8', function (err) {
+	if (err) {
+	    throw err;
+	} 
+	
+	self.log('Ok! The bower.json is writed with successful.');
+    });
+}
+
+/**
+ * Writhe the package.json in the root of your module
+ */
+function writeNPMJson() {
+    this.fs.copyTpl(
+	this.templatePath('package.json'),
+	this.destinationPath('package.json'),
+	{module: this.module}
+    );
+}
+
 module.exports = generators.Base.extend({
-    initializing: function () {
+    constructor: function () {
+	generators.Base.apply(this, arguments);
+	
+	// All options setted by the user
+	this.module = {};
+
+	this.generateDependencies = generateDependencies;
+	this.writeBowerJson = writeBowerJson;
+	this.writeNPMJson = writeNPMJson;
+    },
+
+    prompting: function () {
+	return this.prompt([{
+	    type: 'input',
+	    name: 'name',
+	    message: 'Your Angular module name',
+	    default: this.appname
+	}, {
+	    type: 'input',
+	    name: 'description',
+	    message: 'Description of the your module'
+	}, {
+	    type: 'input',
+	    name: 'license',
+	    message: 'License',
+	    default: 'ISC'
+	}]).then(function (answer) {
+	    this.module = answer;
+	}.bind(this));
+    },
+
+    configuring: function () {
+	this.writeBowerJson();
+	this.writeNPMJson();
+
+	// Generate the karma.conf.js file
 	this.composeWith(
 	    'karma:app', 
 	    {
 		options: {
+		    'test-files': this.generateDependencies(),
 		    'template-path': __dirname + '/templates',
 		    'config-path': '.'
 		}
-	    }, 
+	    },
 	    require.resolve('generator-karma/generators/app/index.js')
 	)
+    },
+
+    install: function () {
+	this.installDependencies();
     }
 });
